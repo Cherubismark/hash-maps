7509e7929a01b06f63f22d81a01374fc411f9638
Cherubismark <bismark.ckoskei@gmail.com>
2026-06-10 10:06:44 +0300

    commenting the code

--- TwoSum.java
+++ TwoSum.java
@@ -4,9 +4,15 @@ public class TwoSum {
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
     public int[] twoSum(int[] nums, int target){
+
+        // Pick the first number
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
+            
+         // Compare it with every number after it
+    for (int j = i + 1; j < nums.length; j++) {
+                // Check if the pair adds up to the target
+                if(nums[i] + nums[j] == target){
+                    // Return the indices of the matching pair
                     return new int[]{i, j};
                 }
 
@@ -14,8 +20,7 @@ public class TwoSum {
 
         }
 
-
-
+        // Return empty array if no solution is found
         return new int[]{};
     }
     
